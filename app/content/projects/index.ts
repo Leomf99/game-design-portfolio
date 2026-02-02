a0777df9329f692354d0f56e733735f8cdb22e1f
Leonardo Martínez <leonardo.martinez@ucarolina.edu.mx>
2026-02-02 16:40:35 -0600

    Projects added

--- app/content/projects/index.ts
+++ app/content/projects/index.ts
@@ -1,7 +1,17 @@
-import { imaginacionFgs } from "./imaginacion-fgs";
 import { notMyWar } from "./not-my-war";
 import { desiertoMagico } from "./desierto-magico";
+import { imaginacionFgs } from "./imaginacion-fgs";
+import { eraDeHielo } from "./era-de-hielo";
+import { odisea } from "./odisea";
+import { dpt82 } from "./dpt-82";
 
-const allProjects = [notMyWar, imaginacionFgs, desiertoMagico];
+const allProjects = [
+  notMyWar,
+  imaginacionFgs,
+  desiertoMagico,
+  eraDeHielo,
+  odisea,
+  dpt82,
+];
 
 export const projects = allProjects.filter((project) => project.published);
